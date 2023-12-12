524da2873561c310261090b9c5d64f2c3fc00cb8
Crazy Luney <k.govzich@gmail.com>
2023-12-12 20:32:23 +0300

    Added enroth module (initial)

---
+++ modules/enroth/register_types.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "modules/register_module_types.h"
+
+void initialize_enroth_module(ModuleInitializationLevel p_level);
+void uninitialize_enroth_module(ModuleInitializationLevel p_level);
